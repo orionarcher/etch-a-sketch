68ef4dc87257bd3a819e7cc89007ef70806f2df0
orioncohen <orion@lbl.gov>
2021-09-05 10:50:54 -0700

    updated game to feature reset button

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
-let grid_size;
-do {
-    grid_size = prompt("How large would you like the grid to be (1-80)?")
-    grid_size = parseInt(grid_size);
-} while (grid_size < 0 || grid_size > 40);
-
-
+grid_size = getGridSize()
 let grid_container = document.querySelector("#grid-container");
-grid_container.style.gridTemplateColumns = "1fr ".repeat(grid_size);
 
-for (i = 0; i < grid_size ** 2; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('class', 'etch-box')
-    grid_container.appendChild(div);
+createGrid(grid_container, grid_size);
+
+
+function getGridSize () {
+    let grid_size;
+    do {
+        grid_size = prompt("How large would you like the grid to be (1-80)?")
+        grid_size = parseInt(grid_size);
+    } while (grid_size < 0 || grid_size > 40);
+    return grid_size
 }
 
-divs = document.querySelectorAll('.etch-box')
+function createGrid(grid_container, grid_size) {
+    grid_container.style.gridTemplateColumns = "1fr ".repeat(grid_size);
 
-divs.forEach(div => { 
-    div.addEventListener('mouseenter', changeColor)
-});
+    for (i = 0; i < grid_size ** 2; i++) {
+        const div = document.createElement('div');
+        div.setAttribute('class', 'etch-box')
+        grid_container.appendChild(div);
+    }
+    divs = document.querySelectorAll('.etch-box')
+    
+    divs.forEach(div => { 
+        div.addEventListener('mouseenter', changeColor)
+    });
+}
+
 
 function changeColor(e) {
     this.classList.add('hovered')
 }
 
+function resetGrid() {
+    gridSize = getGridSize()
+    removeAllChildNodes(grid_container)
+    createGrid(grid_container, gridSize)
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
